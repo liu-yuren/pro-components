842221c9298ad9c41be002cfa41d687b6b80c03f
liuyuren <1540979440@qq.com>
2025-08-25 20:36:29 +0800

    feat: 2

--- play/src/main.ts
+++ play/src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
-// import ProComponents from 'pro-ui'
+import ProComponents from '../../dist/lib.es.js'
 
 import App from './App.vue'
 
 const app = createApp(App)
-// app.use(ProComponents)
+app.use(ProComponents)
 app.mount('#app')
